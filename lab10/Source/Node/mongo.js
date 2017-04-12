fe95430063231b71f6b5524ed40270fad9c010e4
pareshkasare <pareshkasare@gmail.com>
2017-04-12 02:03:34 -0500

    inc 3 commit

--- lab10/Source/Node/mongo.js
+++ lab10/Source/Node/mongo.js
@@ -16,15 +16,105 @@ app.post('/register', function (req, res) {
             res.write("Failed, Error while connecting to Database");
             res.end();
         }
-        insertDocument(db, req.body, function() {
-            res.write("Successfully inserted");
+        insertDocument(db, req.body,res, function() {
+
+
+        });
+    });
+});
+app.post('/updateUser', function (req, res) {
+    MongoClient.connect(url, function(err, db) {
+        if(err)
+        {
+            res.write("Failed, Error while connecting to Database");
+            res.end();
+        }
+        updateDocument(db, req.body,res, function() {
+
+
+        });
+    });
+});
+app.post('/login', function (req, res) {
+    MongoClient.connect(url, function(err, db) {
+        if(err)
+        {
+            res.write("Failed, Error while connecting to Database");
+            res.end();
+        }
+        loginuser(db, req.body,res, function() {
+            //res.write("success");
             db.close();
             res.end();
         });
     });
-})
-var insertDocument = function(db, data, callback) {
-    db.collection('users').insertOne( data, function(err, result) {
+});
+app.post('/table', function (req, res) {
+    MongoClient.connect(url, function(err, db) {
+        if(err)
+        {
+            res.write("Failed, Error while connecting to Database");
+            res.end();
+        }
+        getusers(db, res, function() {
+            //res.write("success");
+            //console.log(res);
+            db.close();
+            res.end();
+        });
+    });
+});
+app.post('/removeuser', function (req, res) {
+    MongoClient.connect(url, function(err, db) {
+        if(err)
+        {
+            res.write("Failed, Error while connecting to Database");
+            res.end();
+        }
+        removeuser(db, req.body,res, function() {
+            res.write("Success");
+            //console.log(res);
+            db.close();
+            res.end();
+        });
+    });
+});
+var insertDocument = function(db, data,res, callback) {
+
+    console.log("em:" + data.email);
+    var cursor = db.collection('users').find({"email":data.email});
+    cursor.each(function(err,doc){
+        //assert.equal(err,null);
+        console.log("data"+ JSON.stringify(doc));
+        if(doc !== null)
+        {
+            res.write("Failure");
+            db.close();
+            res.end();
+            return false;
+        }
+        else {
+            db.collection('users').insertOne( data, function(err, result) {
+                if(err)
+                {
+                    res.write("DBINSERT failure");
+                    db.close();
+                    res.end();
+
+                }
+                else{
+                    res.write("Success");
+                    db.close();
+                    res.end();
+                }
+
+            });
+            return false;
+        }
+
+    });callback();
+
+    /*db.collection('users').insertOne( data, function(err, result) {
         if(err)
         {
             res.write("Registration Failed, Error While Registering");
@@ -32,8 +122,98 @@ var insertDocument = function(db, data, callback) {
         }
         console.log("Inserted a document into the users collection.");
         callback();
+    });*/
+};
+var updateDocument = function(db, data,res, callback) {
+
+    console.log("DATA FOR UPDATE::::::" +JSON.stringify(data));
+    var ObjectId = require('mongodb').ObjectID;
+    db.collection('users').updateOne( {_id:ObjectId(data.id)},{$set:{fname:data.fname,lname:data.lname,email:data.email,password:data.pw}}, function(err, result) {
+     if(err)
+     {
+     res.write("DBUPDATE failure");
+     res.end();
+     }
+     else{
+         res.write("Success");
+         db.close();
+         res.end();
+     }
+     callback();
+     });
+};
+var loginuser = function(db, data,res, callback) {
+    console.log("loggin in user"+data.password);
+    var cursor = db.collection('users').find({"email":data.email,"password":data.password});
+    cursor.each(function(err,doc){
+        assert.equal(err,null);
+        console.log("data"+ data);
+        if(doc != null)
+        {
+            res.write("success")
+            console.log("User logged:" + doc.email);
+            console.log("pwd:"+doc.password);
+        }
+        else {
+            callback();
+        }
     });
 };
+var getusers = function(db, res, callback) {
+    //console.log("loggin in user"+data.password);
+    //var cursor = db.collection('users').find();
+
+    var stream = db.collection('users').find().stream();
+    var out="";
+    stream.on('data', function (doc) {
+
+        console.log(JSON.stringify(doc));
+        out = out+JSON.stringify(doc)+",";
+
+    });
+    stream.on('end', function() {
+        out = "["+out.slice(0,-1)+"]";
+        res.write(out);
+
+        callback();
+    });
+};
+var removeuser = function(db, data,res, callback) {
+    console.log("id passed"+data.id);
+    var ObjectId = require('mongodb').ObjectID;
+
+    var cursor = db.collection('users').remove({_id:ObjectId(data.id)});
+    MongoClient.connect(url, function(err, db) {
+        if(err)
+        {
+            res.write("Failed, Error while connecting to Database");
+            //res.end();
+        }
+       /* getusers(db, res, function() {
+            //res.write("success");
+            //console.log(res);
+            //db.close();
+            //res.end();
+        });*/
+    });
+    callback();
+    /*
+    cursor.each(function(err,doc){
+        assert.equal(err,null);
+        console.log("data"+ data);
+        if(doc != null)
+        {
+            res.write("success")
+            console.log("User logged:" + doc.email);
+            console.log("pwd:"+doc.password);
+        }
+        else {
+            callback();
+        }
+    });*/
+
+};
+
 var server = app.listen(8081, function () {
     var host = server.address().address
     var port = server.address().port
--- lab10/Source/js/index.js
+++ lab10/Source/js/index.js
@@ -5,7 +5,19 @@ myapp.run(function ($http) {
     // Send this header only in post requests. Specifies you are sending a JSON object
     $http.defaults.headers.post['dataType'] = 'json'
 });
+/*myapp.config(function($routeProvider)
+{
+    $routeProvider
+
+    // route for the home page
+        .when('/showusers', {
+            templateUrl : 'Source/showusers.html',
+            controller  : 'Mongotablecontroller'
+        });
+});*/
 myapp.controller('MongoRestController',function($scope,$http,$window){
+    console.log("rest called:");
+
     $scope.formDataerror = "";
     $scope.insertData = function(){
 
@@ -20,8 +32,7 @@ myapp.controller('MongoRestController',function($scope,$http,$window){
         }
         console.log(validform);
         if(validform) {
-
-
+            console.log(JSON.stringify($scope.formData));
             console.log($scope.formData.lname);
             console.log($scope.formData.fname);
             console.log($scope.formData.email);
@@ -37,12 +48,21 @@ myapp.controller('MongoRestController',function($scope,$http,$window){
                 headers: {
                     'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
                 }
-            }
+            };
             var req = $http.post('http://127.0.0.1:8081/register', $scope.formData);
             req.success(function (data, status, headers, config) {
 
                 console.log(data);
-                $window.location.href = "showusers.html";
+                if(data === "Success"){
+                    $window.location.href = "login.html";
+                }else if(data === "DBINSERT failure"){
+                    alert("registration Failed, DB error!!!");
+                }
+                else{
+                    alert("registration Failed, User already exists!!!");
+                }
+
+                //$window.location.href = "showusers.html";
 
             });
             req.error(function (data, status, headers, config) {
@@ -51,4 +71,160 @@ myapp.controller('MongoRestController',function($scope,$http,$window){
         }
 
     };
+});
+myapp.controller('MongologinController',function($scope,$http,$window){
+    $scope.formDataerror = "";
+    $scope.insertData = function(){
+
+    console.log($scope.formData.email);
+    console.log($scope.formData.password);
+
+    var dataParams = {
+
+        'email': $scope.email,
+        'pw': $scope.pw
+    };
+    var config = {
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+        }
+    }
+    console.log("sending req");
+    var req = $http.post('http://127.0.0.1:8081/login', $scope.formData);
+
+        req.success(function (data, status, headers, config) {
+            console.log("found success");
+            console.log(data);
+
+            if(data === "success"){
+
+                console.log("found the user");
+                $window.location.href = "showusers.html";
+            }
+            else{
+                $scope.formDataerror = "Login failed!!! try registering?";
+            }
+        });
+        req.error(function (data, status, headers, config) {
+            alert("failure message: " + JSON.stringify({data: data}));
+        });
+
+
+    };
+});
+myapp.controller('MongotableController',function($scope,$http,$window){
+    console.log("Controller called");
+
+    $scope.removeUser = function(userid,email){
+
+
+        var dataParams = {
+
+            'id': userid
+        };
+        var config = {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+            }
+        }
+        console.log("sending req");
+        var req = $http.post('http://127.0.0.1:8081/removeuser', {"id":userid});
+
+        req.success(function (data, status, headers, config) {
+            console.log("found success");
+            console.log(data);
+
+            if(data === "Success"){
+                function findAndRemove(array, property, value) {
+                    array.forEach(function(result, index) {
+                        if(result[property] === value) {
+                            //Remove from array
+                            array.splice(index, 1);
+                        }
+                    });
+                }
+
+                findAndRemove($scope.users,'email', email);
+
+
+            }
+            else{
+                $scope.formDataerror = "Login failed!!! try registering?";
+            }
+        });
+        req.error(function (data, status, headers, config) {
+            alert("failure message: " + JSON.stringify({data: data}));
+        });
+
+
+    };
+
+    $scope.updateUser = function(id,fname,lname,email,pwd){
+
+        console.log("inside update");
+            console.log(lname);
+            console.log(id);
+            console.log(fname);
+            console.log(email);
+            console.log(pwd);
+        var dataParams = {
+            'id':id,
+            'fname': fname,
+            'lname': lname,
+            'email': email,
+            'pw': pwd
+        };
+            var config = {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+                }
+            };
+            var req = $http.post('http://127.0.0.1:8081/updateUser', dataParams);
+            req.success(function (data, status, headers, config) {
+
+                console.log(data);
+                if(data === "Success"){
+
+                }else if(data === "DBINSERT failure"){
+                    alert("registration Failed, DB error!!!");
+                }
+                else{
+                    alert("registration Failed, User already exists!!!");
+                }
+
+                //$window.location.href = "showusers.html";
+
+            });
+            req.error(function (data, status, headers, config) {
+                alert("failure message: " + JSON.stringify({data: data}));
+            });
+
+
+    };
+
+
+        var config = {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+            }
+        }
+        console.log("sending req");
+        var req = $http.post('http://127.0.0.1:8081/table');
+
+        req.success(function (data, status, headers, config) {
+            console.log("found success");
+            console.log(data);
+
+            if (data) {
+
+                $scope.users = data;
+            }
+            else {
+                $scope.formDataerror = "Login failed!!! try registering?";
+            }
+        });
+        req.error(function (data, status, headers, config) {
+            alert("failure message: " + JSON.stringify({data: data}));
+        });
+
 });
